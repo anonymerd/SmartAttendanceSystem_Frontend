38d4b66bd7d21cfdf2cd0621b676baa3173daf34
kv2905 <keshavv361@gmail.com>
2022-05-02 01:52:53 +0530

    ui changes

--- src/Employee/Employee.jsx
+++ src/Employee/Employee.jsx
@@ -22,168 +22,221 @@ const getCurrDateTime = () => {
   );
 };
 
-const clientsInfo = {
-  headers: ['Id', 'Organization Name', 'Admin Name', 'No. of Employees'],
-  rows: [
-    {
-      id: 420,
-      name: 'Ambazon',
-      admin: 'Luxmon',
-      noOfEmployees: 69,
-    },
-    {
-      id: 421,
-      name: 'Goomgle',
-      admin: 'MonLux',
-      noOfEmployees: 56,
-    },
-    {
-      id: 422,
-      name: 'Samsoong',
-      admin: 'Kemshav',
-      noOfEmployees: -39,
-    },
-    {
-      id: 423,
-      name: 'KPMG',
-      admin: 'Amkhil',
-      noOfEmployees: 64545646542,
-    },
-  ],
-};
+// const clientsInfo = {
+//   headers: ['Id', 'Organization Name', 'Admin Name', 'No. of Employees'],
+//   rows: [
+//     {
+//       id: 420,
+//       name: 'Ambazon',
+//       admin: 'Luxmon',
+//       noOfEmployees: 69,
+//     },
+//     {
+//       id: 421,
+//       name: 'Goomgle',
+//       admin: 'MonLux',
+//       noOfEmployees: 56,
+//     },
+//     {
+//       id: 422,
+//       name: 'Samsoong',
+//       admin: 'Kemshav',
+//       noOfEmployees: -39,
+//     },
+//     {
+//       id: 423,
+//       name: 'KPMG',
+//       admin: 'Amkhil',
+//       noOfEmployees: 64545646542,
+//     },
+//   ],
+// };
 
-const newReqs = {
-  headers: [
-    'Organization Name',
-    'Email',
-    'No. of Employees',
-    'Date/Time',
-    'Action',
-  ],
-  rows: [
-    {
-      name: 'Philipcart',
-      email: 'info@philipcart.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'MacroHard',
-      email: 'info@macrohard.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'PayCM',
-      email: 'info@paycm.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-    {
-      name: 'Desi Katta',
-      email: 'jaankaari@desikatta.com',
-      noOfEmployees: 88,
-      dateTime: getCurrDateTime(),
-      action: (
-        <div>
-          <Button>Approve</Button> <Button>Deny</Button>
-        </div>
-      ),
-    },
-  ],
-};
+// const newReqs = {
+//   headers: [
+//     'Organization Name',
+//     'Email',
+//     'No. of Employees',
+//     'Date/Time',
+//     'Action',
+//   ],
+//   rows: [
+//     {
+//       name: 'Philipcart',
+//       email: 'info@philipcart.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'MacroHard',
+//       email: 'info@macrohard.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'PayCM',
+//       email: 'info@paycm.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//     {
+//       name: 'Desi Katta',
+//       email: 'jaankaari@desikatta.com',
+//       noOfEmployees: 88,
+//       dateTime: getCurrDateTime(),
+//       action: (
+//         <div>
+//           <Button>Approve</Button> <Button>Deny</Button>
+//         </div>
+//       ),
+//     },
+//   ],
+// };
+
+const data = [
+  {
+    EmpId: 1,
+    Name: 'Rohit Bisht',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 2,
+    Name: 'Keshav',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 3,
+    Name: 'Akhil',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 4,
+    Name: 'Jane Doe',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 1,
+    Name: 'Rohit Bisht',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 2,
+    Name: 'Keshav',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 3,
+    Name: 'Akhil',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+  {
+    EmpId: 4,
+    Name: 'Jane Doe',
+    CheckIn: '20-08-2365',
+    CheckOut: '36-89-1265',
+  },
+];
+
+const headers = ['EmpId', 'Name', 'CheckIn', 'CheckOut'];
 
 const employee = {
   name: 'Akhil Kumar',
@@ -203,7 +256,7 @@ const Employee = () => {
             <PersonCard data={employee}/>
           </div>
         <div className='new-request-container'>
-          <Table headers={newReqs.headers} rows={newReqs.rows} />
+        <Table headers={headers} rows={data} />
         </div>
         </section>
       </div>
